4cd33b476d6449e33fdd9116125f053444ca9065
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2020-10-14 14:17:23 +0300

    Delete Steenee.java

--- Steenee.java
+++
@@ -1,9 +0,0 @@
-import ru.ifmo.se.pokemon.*;
-
-public class Steenee extends Bounsweet{
-	public Steenee(String name, int level){
-		super(name, level);
-		setStats(52, 40, 48, 40, 48, 62);
-		addMove(new PlayNice());
-	}
-}
